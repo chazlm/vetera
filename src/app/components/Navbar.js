110d6c010c08c07f0f62361d6fd61c33a65c4a6a
chaz <chaz60795@yahoo.com>
2024-09-04 22:07:11 -0400

    add navbar

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,96 @@
+"use client";
+
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  InputBase,
+  Box,
+  Button,
+} from "@mui/material";
+import { styled, alpha } from "@mui/material/styles";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+
+const Navbar = () => {
+  return (
+    <AppBar color="transparent" position="static" sx={{ paddingTop: "2rem" }}>
+      <Toolbar>
+        {/* Menu Icon */}
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+
+        {/* Logo */}
+        <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+          Reddit Clone
+        </Typography>
+
+        {/* Navigation Links */}
+        <Box sx={{ display: { xs: "none", md: "flex" } }}>
+          <Button color="inherit">Home</Button>
+          <Button color="inherit">Popular</Button>
+          <Button color="inherit">Explore</Button>
+        </Box>
+
+        {/* Search Bar */}
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Search…"
+            inputProps={{ "aria-label": "search" }}
+          />
+        </Search>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
